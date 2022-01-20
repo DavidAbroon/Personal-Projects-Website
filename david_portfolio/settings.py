dc7d730d739ea8a1d2ab5471e65ab79f738d93d0
DavidAbroon <rocah97@gmail.com>
2022-01-21 10:03:06 +1100

    added blog post

--- david_portfolio/settings.py
+++ david_portfolio/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True 
 
-ALLOWED_HOSTS = [".learnwithabroon.com", "127.0.0.1:8000"]
+ALLOWED_HOSTS = [".learnwithabroon.com", "127.0.0.1"]
 
 
 # Application definition
--- david_portfolio/urls.py
+++ david_portfolio/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('1013934asdkasdjasdk/', admin.site.urls),
     path("", include("projects.urls")),
     path("projects/", include("projects.urls")),
     path("blog/", include("blog.urls")),
